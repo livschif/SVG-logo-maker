566d869035ccb38743f3c80f1113a918444b453c
Olivia Schif <131719296+livschif@users.noreply.github.com>
2023-08-21 15:57:06 -0500

    add all files needed for project

---
+++ index.js
@@ -0,0 +1,6 @@
+// Inquirer import
+const inquirer = require('inquirer');
+// File system import
+const fs = require('fs');
+// Shape classes from ./lib directory
+const { Triangle, Squre, Cirle } = require('./lib');
